7e88236df166df90f9a3053e3057ff1dc0e4b9ea
kdtTetz <xenosign@naver.com>
2023-02-16 00:52:44 +0900

    230216 수업 준비

--- js/main.js
+++ js/main.js
@@ -4,25 +4,44 @@ const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 const searchIconEl = searchEl.querySelector("span");
 
-let isFocused = false;
+// 외부 요소를 클릭하여 포커스 아웃 된 경우를 처리하기 위한 전역 변수
+let isNotSearchClick = false;
+
+// 돋보기 아이콘이 아닌 외부 요소를 클릭하였는지를 확인 하기 위하여
+// 문서에 이벤트 리스너 걸기
+document.addEventListener("click", function (e) {
+  // 요소를 클릭한게 아니라면 --> 외부를 클릭하여 포커스 아웃이 되었다면, 전역 변수를 true;
+  if (!e.target.classList.contains("material-symbols-outlined")) {
+    isNotSearchClick = true;
+    // 요소를 클릭했다면 --> 전역 변수를 false;
+  } else {
+    isNotSearchClick = false;
+  }
+});
 
 searchIconEl.addEventListener("click", function () {
-  searchInputEl.focus();
-  console.log(document.activeElement);
+  // 클릭이 한번도 안된 상태라서 active 클래스가 없다면?
+  // active 클래스 부여 및 focus 보내기, placeholder 지정
+  if (!searchEl.classList.contains("active")) {
+    searchEl.classList.add("active");
+    searchInputEl.focus();
+    searchInputEl.setAttribute("placeholder", "통합 검색");
 
-  // if (isFocused) {
-  //   searchInputEl.blur();
-  //   isFocused = false;
-  // } else {
-  //   searchInputEl.focus();
-  //   isFocused = true;
-  // }
+    // 외부 요소 클릭으로 포커스가 아웃 된 상태에서, 다시 돋보기를 누르면
+    // active 클래스 부여 및 focus 보내기, placeholder 지정
+  } else if (isNotSearchClick) {
+    searchInputEl.focus();
+    searchInputEl.setAttribute("placeholder", "통합 검색");
+    isNotSearchClick = false;
+
+    // active 클래스와, focus 가 모두 있는 상태에서 클릭을 하면 active 클래스를 제거하여
+    // 토글 효과
+  } else {
+    searchEl.classList.remove("active");
+  }
 });
 
-searchInputEl.addEventListener("focus", function () {
-  searchInputEl.setAttribute("placeholder", "통합 검색");
-});
-
-searchInputEl.addEventListener("blur", function () {
+// 포커스 아웃이 되면 일단 placeholder 초기화
+searchInputEl.addEventListener("blur", function (e) {
   searchInputEl.setAttribute("placeholder", "");
 });
--- tt.js
+++ tt.js
@@ -1,18 +1,38 @@
-searchIconEl.addEventListener("click", function () {
-  if (isFocused) {
-    searchInputEl.blur();
-    isFocused = false;
+// MAIN HEADER
+// SEARCH
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+const searchIconEl = searchEl.querySelector("span");
+
+let isNotSearchClick = false;
+let clickEl;
+
+document.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("material-symbols-outlined")) {
+    isNotSearchClick = true;
   } else {
-    searchInputEl.focus();
-    isFocused = true;
+    isNotSearchClick = false;
   }
 });
 
-searchInputEl.addEventListener("focus", function () {
-  searchInputEl.setAttribute("placeholder", "통합 검색");
+searchIconEl.addEventListener("click", function () {
+  if (isNotSearchClick) {
+    console.log("1");
+    searchEl.classList.add("active");
+    searchInputEl.focus();
+    searchInputEl.setAttribute("placeholder", "통합 검색");
+    isNotSearchClick = false;
+  } else if (!searchEl.classList.contains("active")) {
+    console.log("2");
+    searchEl.classList.add("active");
+    searchInputEl.focus();
+    searchInputEl.setAttribute("placeholder", "통합 검색");
+  } else {
+    console.log("3");
+    searchEl.classList.remove("active");
+  }
 });
 
-searchInputEl.addEventListener("blur", function () {
+searchInputEl.addEventListener("focusout", function (e) {
   searchInputEl.setAttribute("placeholder", "");
 });
-
